cea86d73b63600cd6a9df9e0943760b4a70a14dd
AJ de Leon <ajdeleon1064@gmail.com>
2018-01-17 00:55:23 -0500

    add logic for increase decrease breakTime and sessionTime

--- src/components/pomodoro/Button.js
+++ src/components/pomodoro/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Button = ({ sign }) => {
-  return <a className="btn-floating btn-small waves-effect waves-light red"><i className="material-icons">{sign}</i></a>
+const Button = ({ sign, handleClick }) => {
+  return <a className="btn-floating btn-small waves-effect waves-light red" onClick={handleClick}><i className="material-icons">{sign}</i></a>
 }
 
 export default Button
--- src/components/pomodoro/Pomodoro.js
+++ src/components/pomodoro/Pomodoro.js
@@ -10,19 +10,40 @@ class Pomodoro extends Component {
     session: 25
   }
 
+  sessionChange = (sign) => {
+    if (this.state.sessionTime) {
+      this.setState((state) => {
+        return sign === 'plus' ? {sessionTime: state.sessionTime + 1} : {sessionTime: state.sessionTime -1}
+      })
+    }
+  }
+
+  breakChange = (sign) => {
+    if (this.state.breakTime) {
+      this.setState((state) => {
+        return sign === 'plus' ? {breakTime: state.breakTime + 1} : {breakTime: state.breakTime -1}
+      })
+    }
+  }
+
   render() {
     return(
       <div className="container main-pomodoro">
         <div className="row controls">
           <div className="col s6 center">
-            <Button sign="remove" />
+            <Button sign="remove" handleClick={() => this.breakChange('minus')}/>
             {this.state.breakTime}
-            <Button sign="add" />
+            <Button sign="add" handleClick={() => this.breakChange('plus')}/>
           </div>
           <div className="col s6 center">
-            <Button sign="remove" />
+            <Button sign="remove" handleClick={() => this.sessionChange('minus')}/>
             {this.state.sessionTime}
-            <Button sign="add" />
+            <Button sign="add" handleClick={() => this.sessionChange('plus')}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s6 offset-s3 center">
+            Start
           </div>
         </div>
         <div className="row">
