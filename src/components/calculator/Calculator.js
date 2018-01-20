675cf93d75b4dc4d6d55b80cef68e7c172cb5f3c
AJ de Leon <ajdeleon1064@gmail.com>
2018-01-19 22:00:33 -0500

    add calculator style scaffold

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import './Calculator.css'
 
 const Calculator = () => {
-  return <h1>Calculator Placeholder</h1>
+  return (
+    <div className="Calculator-wrapper">
+      <div className="Calculator-display">
+        <span>DISPLAY</span>
+      </div>
+      <div className="Calculator-functions">
+        <div>AC</div>
+        <div>±</div>
+        <div>%</div>
+      </div>
+      <div className="Calculator-operations">
+        <div>÷</div>
+        <div>x</div>
+        <div>-</div>
+        <div>+</div>
+        <div>=</div>
+      </div>
+      <div className="Calculator-keypad">
+        <div className="zero">0</div>
+        <div className="period">.</div>
+        <div className="one">1</div>
+        <div className="two">2</div>
+        <div className="three">3</div>
+        <div className="four">4</div>
+        <div className="five">5</div>
+        <div className="six">6</div>
+        <div className="seven">7</div>
+        <div className="eight">8</div>
+        <div className="nine">9</div>
+      </div>
+    </div>
+  )
 }
 
 export default Calculator
