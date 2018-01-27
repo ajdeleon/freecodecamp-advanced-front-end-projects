95cba4ea5ded63585e6e7f8b8da3d1a720eb1e67
AJ de Leon <ajdeleon1064@gmail.com>
2018-01-27 14:47:31 -0500

    redesign game layout using flexbox instead of css grid

--- src/components/simon/Simon.js
+++ src/components/simon/Simon.js
@@ -4,10 +4,16 @@ import './Simon.css'
 const Simon = () => {
   return (
     <div className="Simon-container">
-      <div className="game-button"></div>
-      <div className="game-button"></div>
-      <div className="game-button"></div>
-      <div className="game-button"></div>
+      <div className="first-row">
+        <div className="game-button" id="one"></div>
+      </div>
+      <div className="second-row">  
+        <div className="game-button" id="two"></div>
+        <div className="game-button" id="three"></div>
+      </div>
+      <div className="third-row">
+        <div className="game-button" id="four"></div>
+      </div>
     </div>
   )
 }
