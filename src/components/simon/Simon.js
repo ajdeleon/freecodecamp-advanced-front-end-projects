ce0cbf4857bdf85a83802e8e12ce522a713d79dc
AJ de Leon <ajdeleon1064@gmail.com>
2018-01-28 23:08:47 -0500

    outline and begin logic for simon game

--- src/components/simon/Simon.js
+++ src/components/simon/Simon.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Simon.css'
 
-const Simon = () => {
-  return (
-    <div className="Simon-container">
-      <div className="first-row">
-        <div className="game-button" id="one"></div>
+class Simon extends Component {
+  state = {
+    playerMoves: [],
+    gameMoves: []
+  }
+
+  playerMove(button) {
+    //flash color of button
+    //--add value to player move
+    //compare it to game move
+    //if good, perform next game move
+    //if not, end game
+    this.setState((state) => {
+      return {
+        playerMoves: [...state.playerMoves, button]
+      }
+    })
+  }
+
+  renderList() {
+    let temp = this.state.playerMoves
+    console.log(temp)
+    return temp.map(x => {
+      return <div>{x}</div>
+    })
+  }
+
+  render() {
+    return (
+      <div className="Simon-container">
+        <div className="first-row">
+          <div onClick={() => this.playerMove("top")}className="game-button" id="top"></div>
+        </div>
+        <div className="second-row">  
+          <div onClick={() => this.playerMove("left")}className="game-button" id="left"></div>
+          <div onClick={() => this.playerMove("right")}className="game-button" id="right"></div>
+        </div>
+        <div className="third-row">
+          <div onClick={() => this.playerMove("bottom")}className="game-button" id="bottom"></div>
+        </div>
+        {this.renderList()}
       </div>
-      <div className="second-row">  
-        <div className="game-button" id="two"></div>
-        <div className="game-button" id="three"></div>
-      </div>
-      <div className="third-row">
-        <div className="game-button" id="four"></div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Simon
