ee04f1d11ef8b57f6ff06ace6e12c787bc60a692
AJ de Leon <ajdeleon1064@gmail.com>
2018-01-29 23:54:48 -0500

    add logic to compare game moves and player moves and either move on or end game

--- src/components/simon/Simon.js
+++ src/components/simon/Simon.js
@@ -1,30 +1,58 @@
 import React, { Component } from 'react'
 import './Simon.css'
+import '../../utils/arrayEquals'
 
 class Simon extends Component {
   state = {
     playerMoves: [],
-    gameMoves: []
+    gameMoves: ["left"]
+  }
+
+  showgGameMoves() {
+    let moves = this.state.gameMoves
+    return moves.map(x => {
+      return console.log(x)
+    })
   }
 
   playerMove(button) {
     //flash color of button
     //--add value to player move
-    //compare it to game move
-    //if good, perform next game move
-    //if not, end game
-    this.setState((state) => {
-      return {
-        playerMoves: [...state.playerMoves, button]
-      }
+    //--compare it to game move
+    //--if good, perform next game move
+    //--if not, end game
+    this.setState({
+        playerMoves: [...this.state.playerMoves, button]
+      }, () => {
+          if (this.state.playerMoves.equals(this.state.gameMoves)) {
+            if (this.state.playerMoves.length === this.state.gameMoves.length) {
+              alert("Good")
+              this.setState({
+                playerMoves: []
+              }, this.gameMove())
+            }
+          } else {
+            alert("Bad. Restarting game!")
+            this.setState({
+              playerMoves: [],
+              gameMoves: ["left"]
+            })
+          }
+      })
+  }
+
+  gameMove() {
+    const choices = ["top", "right", "bottom", "left"]
+    let randomNumber = Math.floor((Math.random() * 4) + 0)
+    this.setState({
+      gameMoves: [...this.state.gameMoves, choices[randomNumber]]
     })
   }
 
   renderList() {
-    let temp = this.state.playerMoves
-    console.log(temp)
-    return temp.map(x => {
-      return <div>{x}</div>
+    let temp = this.state.gameMoves
+    return temp.map((x, i) => {
+      return <div key={i}>{x}</div>
     })
   }
 
@@ -32,16 +60,17 @@ class Simon extends Component {
     return (
       <div className="Simon-container">
         <div className="first-row">
-          <div onClick={() => this.playerMove("top")}className="game-button" id="top"></div>
+          <div onClick={() => this.playerMove("top")} ref={top => this.topElement = top} className="game-button" id="top"></div>
         </div>
         <div className="second-row">  
-          <div onClick={() => this.playerMove("left")}className="game-button" id="left"></div>
-          <div onClick={() => this.playerMove("right")}className="game-button" id="right"></div>
+          <div onClick={() => this.playerMove("left")} className="game-button" id="left"></div>
+          <div onClick={() => this.playerMove("right")} className="game-button" id="right"></div>
         </div>
         <div className="third-row">
-          <div onClick={() => this.playerMove("bottom")}className="game-button" id="bottom"></div>
+          <div onClick={() => this.playerMove("bottom")} className="game-button" id="bottom"></div>
         </div>
         {this.renderList()}
+
       </div>
     )
   }
