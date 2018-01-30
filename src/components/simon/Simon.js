d81b71056b270208fcd598fcc03be67cc2f47a34
AJ de Leon <ajdeleon1064@gmail.com>
2018-01-30 13:40:03 -0500

    add display to show current round of game

--- src/components/simon/Simon.js
+++ src/components/simon/Simon.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react'
 import './Simon.css'
 import '../../utils/arrayEquals'
 
-class Simon extends Component {
-  state = {
-    playerMoves: [],
-    gameMoves: ["left"]
-  }
-
-  showgGameMoves() {
-    let moves = this.state.gameMoves
-    return moves.map(x => {
-      return console.log(x)
-    })
-  }
-
-  playerMove(button) {
-    //flash color of button
+   //flash color of button
     //--add value to player move
     //--compare it to game move
     //--if good, perform next game move
     //--if not, end game
+
+class Simon extends Component {
+  state = {
+    playerMoves: [],
+    gameMoves: ["left"],
+    shownMove: null
+  }
+
+  // playGameMoves() {
+  //   let moves = this.state.gameMoves
+
+  //   moves.forEach((x, i) => {
+      
+  //   })
+  // }
+
+  playerMove(button) {
     this.setState({
         playerMoves: [...this.state.playerMoves, button]
       }, () => {
@@ -60,14 +63,15 @@ class Simon extends Component {
     return (
       <div className="Simon-container">
         <div className="first-row">
-          <div onClick={() => this.playerMove("top")} ref={top => this.topElement = top} className="game-button" id="top"></div>
+          <div onClick={() => this.playerMove("top")} ref={top => this.topElement = top} className="game-button top"></div>
         </div>
         <div className="second-row">  
-          <div onClick={() => this.playerMove("left")} className="game-button" id="left"></div>
-          <div onClick={() => this.playerMove("right")} className="game-button" id="right"></div>
+          <div onClick={() => this.playerMove("left")} className="game-button left"></div>
+          <div className="round-display">Round {this.state.gameMoves.length}</div>
+          <div onClick={() => this.playerMove("right")} className="game-button right"></div>
         </div>
         <div className="third-row">
-          <div onClick={() => this.playerMove("bottom")} className="game-button" id="bottom"></div>
+          <div onClick={() => this.playerMove("bottom")} className="game-button bottom"></div>
         </div>
         {this.renderList()}
 
